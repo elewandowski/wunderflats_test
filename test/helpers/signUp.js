f3ddc1b1c355ff6ec002a68757280ca0471b5195
Emil Lewandowski <>
2019-01-23 20:51:00 +0100

    refactor signup helper script

--- test/helpers/signUp.js
+++ test/helpers/signUp.js
@@ -2,43 +2,54 @@ const {user} = require('../data/user.js')
 
 exports.signUp = (email) => {
     const signUpModal = $('.SignupModal')
-    signUpModal.waitForDisplayed()  
+    signUpModal.waitForDisplayed()
 
-    const firstNameField = $('#firstName')
-    firstNameField.waitForExist()
-    firstNameField.setValue(user.firstName)
-    expect(firstNameField.getValue()).to.equal(user.firstName)
+    const fillField = (selector, input) => {
+        $(selector).waitForDisplayed()
+        $(selector).setValue(input)
+    }
 
-    const lastNameField = $('#lastName')
-    lastNameField.waitForExist()
-    lastNameField.setValue(user.lastName)
-    expect(lastNameField.getValue()).to.equal(user.lastName)
+    const loginFields = [
+        {
+            name: 'First Name',
+            selector: '#firstName',
+            input: user.firstName
+        },
+        {
+            name: 'Last Name',
+            selector: '#lastName',
+            input: user.lastName
+        },
+        {
+            name: 'Email',
+            selector: '#email',
+            input: email
+        },
+        {
+            name: 'Phone Number',
+            selector: '.qa-phoneNumber',
+            input: user.telephone
+        },
+        {
+            name: 'Password',
+            selector: '#password',
+            input: user.password
+        },
+        {
+            name: 'Password Confirmation',
+            selector: '#passwordConfirmation',
+            input: user.password
+        }
+    ]
 
-    const emailField = $('#email')
-    emailField.waitForExist()
-    emailField.setValue(email)
-    expect(emailField.getValue()).to.equal(email)
-
-    const telephoneField = $('.CustomPhoneNumberInput-phoneNumber')
-    telephoneField.waitForExist()
-    telephoneField.setValue(user.telephone)
-    expect(telephoneField.getValue()).to.equal(user.telephone)
-
-    const passwordField = $('#password')
-    passwordField.waitForExist()
-    passwordField.setValue(user.password)
-    expect(passwordField.getValue()).to.equal(user.password)
-
-    const passwordConfirmationField = $('#passwordConfirmation')
-    passwordConfirmationField.waitForExist()
-    passwordConfirmationField.setValue(user.password)
-    expect(passwordConfirmationField.getValue()).to.equal(user.password)
+    loginFields.forEach((field) => {
+        fillField(field.selector, field.input)
+    })
 
     const tosField = $('.tos-label input')
     tosField.waitForExist()
     tosField.click()
-    expect(tosField.isSelected()).to.equal(true)
-
+    
     const submitButtonField = $('.Button--primary')
     submitButtonField.waitForExist()
     submitButtonField.click()
